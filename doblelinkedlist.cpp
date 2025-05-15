d979b0184c2953ff065a4d0fa2b96f9f2f137553
upi1682 <upi538287@gmail.com>
2025-05-15 19:02:59 +0700

    c21

--- doblelinkedlist.cpp
+++ doblelinkedlist.cpp
@@ -113,7 +113,8 @@ class DoubleLinkedList
         }
         else
         {
-     
+           //step 3: link previous node to next of current
+           current->prev->next = current->next;
 
        
        }
