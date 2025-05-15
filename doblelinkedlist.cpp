7afd5ec9526923da35aece648246037593bb8283
upi1682 <upi538287@gmail.com>
2025-05-15 19:04:59 +0700

    c29

--- doblelinkedlist.cpp
+++ doblelinkedlist.cpp
@@ -156,8 +156,15 @@ class DoubleLinkedList
             cout << "\nList is empty" << endl;
             return;
         }
- 
-  
+
+        //step 1: Move to last node 
+        Node *currentNode = START;
+        int i = 0;
+        while(currentNode->next != NULL)
+        {
+            currentNode = currentNode->next;
+            i++;
+        }
 
    
     }
