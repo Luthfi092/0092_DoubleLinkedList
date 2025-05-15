537670b4958fc306dea786d86021fa08905d7607
upi1682 <upi538287@gmail.com>
2025-05-15 18:59:08 +0700

    c6

--- doblelinkedlist.cpp
+++ doblelinkedlist.cpp
@@ -42,7 +42,8 @@ class DoubleLinkedList
                 cout << "\nDuplicate number not allowed" << endl;
                 return;
             }
-       
+            //Step 4: newNode.next = START
+            newNode->next = START;
 
 
           
