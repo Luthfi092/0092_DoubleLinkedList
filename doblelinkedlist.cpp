1df66139afb847b8ed99d57913b30450d4c00ffc
upi1682 <upi538287@gmail.com>
2025-05-15 19:00:41 +0700

    c13

--- doblelinkedlist.cpp
+++ doblelinkedlist.cpp
@@ -71,7 +71,9 @@ class DoubleLinkedList
             return;
         }
 
-      
+        //step 9: Insert between current and current->next
+        newNode->next = current->next; //step 9a: newNode.next = current.next
+        newNode->prev = current;       //step 9b: newNode.prev = current
 
 
     }
