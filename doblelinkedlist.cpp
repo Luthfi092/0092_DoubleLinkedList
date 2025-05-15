0dbf274983dd1d027df9e07109cf6054c630ab40
upi1682 <upi538287@gmail.com>
2025-05-15 18:58:14 +0700

    c3

--- doblelinkedlist.cpp
+++ doblelinkedlist.cpp
@@ -31,7 +31,8 @@ class DoubleLinkedList
         //step 1: Allocate memory for new node
         Node *newNode = new Node();
 
-      
+        //step 2: Assign value to the data field
+        newNode->noMhs = nim;
 
 
 
