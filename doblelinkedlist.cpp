61ce978672130fa89519392a53ae66f771cf30a2
upi1682 <upi538287@gmail.com>
2025-05-15 19:08:29 +0700

    c41

--- doblelinkedlist.cpp
+++ doblelinkedlist.cpp
@@ -228,7 +228,16 @@ int main()
 
         switch (choice)
         {
-
+            case '1':
+                list.addNode();
+                break;
+            case '2':
+                list.hapus();
+                break;
+            case '3':
+                list.traverse();
+                break;
+   
         }
    
     } while (choice != '6');   
